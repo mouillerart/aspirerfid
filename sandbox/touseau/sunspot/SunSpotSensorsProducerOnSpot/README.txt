eda9de18bb00df2a2b973655a7bee787e4c604a9
donsez <donsez@3fbdb089-5876-4d40-aeb2-f6ac8130dd8c>
2008-08-29 22:22:10 +0000

    add the on-Spot Sun SPOT sensors producer

---
+++ sandbox/touseau/sunspot/SunSpotSensorsProducerOnSpot/README.txt
@@ -0,0 +1,13 @@
+Sun SPOT Sensors Producer
+
+This project contains the onSPOT Spotlet sensors producer
+The spotlet/midlet reads measurements and listens thresholds on the built-in devices (light detector, temperature, 3D-accelrometer, LEDs, switches) and broadcasts the value in a radio message
+
+Authors: Didier Donsez, Lionel Touseau
+
+TODO License banner
+TODO Host side (Producer and Configuration) (ie OSGi WireAdmin Producer and JMX/CA configurer)
+TODO Maven2 pom.xml
+TODO Ivy ivy.xml
+
+TODO add features inspired from the org.sunspotworld.demo.RadioStrength sample
---
+++ sandbox/touseau/sunspot/SunSpotSensorsProducerOnSpot/src/org/ow2/aspirerfid/sensor/sunspot/SunSpotSensorsProducerOnSpot.java
@@ -0,0 +1,306 @@
+/*
+Banner
+ **/
+
+package org.ow2.aspirerfid.sensor.sunspot;
+
+/*
+ * SunSpotSensorsProducerOnSpot.java
+ *
+ */ 
+
+import com.sun.spot.sensorboard.EDemoBoard;
+import com.sun.spot.sensorboard.io.IIOPin;
+import com.sun.spot.sensorboard.io.IOutputPin;
+import com.sun.spot.sensorboard.peripheral.IAccelerometer3DThresholdListener;
+import com.sun.spot.sensorboard.peripheral.ILightSensorThresholdListener;
+import com.sun.spot.sensorboard.peripheral.ITemperatureInput;
+import com.sun.spot.sensorboard.peripheral.IAccelerometer3D;
+import com.sun.spot.sensorboard.peripheral.ITemperatureInputThresholdListener;
+import com.sun.spot.sensorboard.peripheral.ITriColorLED;
+import com.sun.spot.sensorboard.peripheral.ISwitch;
+import com.sun.spot.sensorboard.peripheral.ILightSensor; 
+import com.sun.spot.sensorboard.peripheral.LEDColor;
+import com.sun.spot.util.Utils;
+import com.sun.spot.util.BootloaderListener;
+
+import java.io.IOException;
+
+import javax.microedition.midlet.MIDlet;
+import javax.microedition.midlet.MIDletStateChangeException;
+
+/**
+ * @author Didier Donsez, Lionel Touseau
+ */ 
+public class SunSpotSensorsProducerOnSpot extends MIDlet
+        implements IAccelerometer3DThresholdListener, ITemperatureInputThresholdListener, ILightSensorThresholdListener {
+
+    
+    private static final String VERSION = "1.0";
+    private static final int INITIAL_CHANNEL_NUMBER = IProprietaryRadio.DEFAULT_CHANNEL;
+    private static final short PAN_ID               = IRadioPolicyManager.DEFAULT_PAN_ID;
+    private static final short MAX_BROADCAST_HOPS=1;      // =1 means "don't want packets being rebroadcasted""
+
+    private static final String BROADCAST_PORT              = "42";
+    /**
+     * Type for the configuration message (host --> spot)
+     */
+    private static final int RADIO_CONFIG_PACKET            = DD;
+    /**
+     * Type for the sensor data message (spot --> spot/host)
+     */
+    private static final int RADIO_SENSORDATA_PACKET        = DE;
+    
+    // indicators
+    private ITriColorLED leds[]          = EDemoBoard.getInstance().getLEDs();
+
+    // outputs
+    //private IOutputPin outputPins        = EDemoBoard.getInstance().getOutputPins();
+
+    // sensors
+    private ISwitch sw1                  = EDemoBoard.getInstance().getSwitches()[0];  
+    private ISwitch sw2                  = EDemoBoard.getInstance().getSwitches()[1];  
+    private ILightSensor lightSensor     = EDemoBoard.getInstance().getLightSensor();  
+    private ITemperatureInput tempSensor = EDemoBoard.getInstance().getADCTemperature();
+    private IAccelerometer3D accelSensor = EDemoBoard.getInstance().getAccelerometer();
+    //private IIOPin ioPins                = EDemoBoard.getInstance().getIOPins();
+    
+    // configuration parameters
+    private boolean indicatorOn = true;
+    private long emissionPeriod = 1000;
+    private double accelerationXThreshold = 20.0d;
+    private double accelerationYThreshold = 20.0d;
+    private double accelerationZThreshold = 20.0d;
+    private double lightMinThreshold = 20.0d;
+    private double lightMaxThreshold = 20.0d;
+    private double temperatureMinThreshold = 273.0d;
+    private double temperatureMaxThreshold = 293.0d;
+    private boolean emissionOnClosedSW1 = true;
+    private boolean emissionOnOpenedSW1 = true;
+    private boolean emissionOnClosedSW2 = true;
+    private boolean emissionOnOpenedSW2 = true;
+    private boolean alertThresholdsOnLED = true;
+    // private double batteryMinThreshold = 20.0d;
+    
+    private void initIndicator(){
+        for(int i = 0; i < leds.length; i++){
+            leds[i].setOn();                    // Enable this LED
+            leds[i].setRGB(255,255,255);              // Set it to black
+        }
+        resetIndicator();
+    }
+
+    private void resetIndicator(){
+        for(int i = 0; i < leds.length; i++){
+            leds[i].setOn();                    // Enable this LED
+            leds[i].setRGB(0,0,0);              // Set it to black
+        }
+        leds[0].setColor(LEDColor.TURQUOISE);   // See LEDColor for more predefined colors.
+    }
+    
+    private void accelIndicator(double accelerationValue){
+        leds[0].setRGB(0, (int) (accelerationValue * accelerationValue * 60.0), 0);  // [0 : 240]
+    }
+   
+    private void lightIndicator(){
+        // TODO
+    }
+    
+    private void heatIndicator(){
+         // heatIndication is scaled so that reaching 20 degrees away from 72 gives the maximum LED brightness (255)
+            int heatIndication = (int) ((tempSensor.getCelsius() - 25.0) * 255.0 / 20);
+            if(heatIndication > 0){
+                leds[0].setRGB(heatIndication, 0, 0);                  //above 25 degrees C (room temp) in red
+            } else {
+                leds[0].setRGB(0, 0, - heatIndication);                //below 25 degrees C (room temp) in blue
+            }
+            
+            int lightIndication = lightSensor.getValue();              //ranges from 0 - 740
+            leds[1].setRGB(0, lightIndication / 3, 0);                 //Set LED green, will range from 0 - 246
+    }
+
+    private void radioConnectionIndicator(){
+        // TODO
+    }
+
+    private void batteryIndicator(){
+        // TODO
+    }
+    
+    private void sendPacketIndicator(boolean ok){
+        
+    }
+    
+    /**
+     * Receive commands to configure the SunSpot
+     */    
+    public void startMessageReceiver() {
+        new Thread() {
+            public void run() {                                
+                while(true){
+                    try {
+                        dg.reset();
+                        dgConnection.receive(dg);
+                        
+                        // TODO test the message header and switch to the method
+                        byte packetType = dg.readByte();
+                        if (packetType == RADIO_CONFIG_PACKET) {
+                            processConfigurationMessage();
+                            sendPacketIndicator(true);
+                        } else {
+                            System.out.println("Unknown message type");                    
+                            sendPacketIndicator(false);
+                        }                   
+                    } catch (IOException e) {
+                        System.out.println("Nothing received");
+                        sendPacketIndicator(false);
+                    }
+                }
+            }
+        }.start();
+    }
+
+    public void processConfigurationMessage() {
+        // read the header (contains the message type)
+
+        // configure the emission period
+        long periodTmp = dg.readLong();
+        if(periodTmp>0) {
+            emissionPeriod=periodTmp;
+        } else {
+            System.out.println("Received a illegal value " + periodTmp + " from " + dg.getAddress());                            
+        }
+
+        // TODO light thredhold
+        // TODO accelSensor thredhold                                               
+    }
+    
+    private void startSensorThresholdListeners(){
+        // TODO
+    }
+    
+    /**
+     * Sends measurement message periodically
+     */
+    synchronized public void startPeriodicalSender() {
+        // TODO should be interupted when the emissionPeriod is changed
+        new Thread() {
+            public void run() {
+                sendSensorDataMessage();
+                Utils.sleep(emissionPeriod);
+            }
+        }.start();
+    }
+    
+    private DatagramConnection dgConnection = null;
+    private Datagram dg = null;
+
+    synchronized private void openDatagramConnection() {
+        // create a DatagramConnection
+        try {
+            // The Connection is a broadcast so we specify it in the creation string
+            dgConnection = (DatagramConnection) Connector.open("radiogram://broadcast:"+BROADCAST_PORT);
+            dgConnection.setMaxBroadcastHops(MAX_BROADCAST_HOPS);
+            // Then, we ask for a datagram with the maximum size allowed
+            dg = dgConnection.newDatagram(dgConnection.getMaximumLength());
+        } catch (IOException ex) {
+            System.out.println("Could not open radiogram broadcast connection");
+            ex.printStackTrace();
+            return;
+        }
+    }
+        
+    /**
+     * Sends measurement message periodically
+     */
+    synchronized private void sendSensorDataMessage() {
+        try {
+            // Fill up the message
+            dg.reset();
+
+            // the message type
+            dg.writeByte(RADIO_SENSORDATA_PACKET);
+            
+            // instantaneous acceleration
+            dg.writeDouble(accelSensor.getAccelX());
+            dg.writeDouble(accelSensor.getAccelY());
+            dg.writeDouble(accelSensor.getAccelZ());
+
+            // instantaneous temperature
+            dg.writeDouble(tempSensor.getCelsius()+273.0);
+
+            // instantaneous light
+            dg.writeInt(lightSensor.getValue());
+
+            // instantaneous switch status
+            dg.writeBoolean(sw1.isOpen());
+            dg.writeBoolean(sw2.isOpen());
+
+            dgConnection.send(dg);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        
+        // TODO blink leds
+        
+    }
+
+    protected void startApp() throws MIDletStateChangeException { 
+        new BootloaderListener().start();       // Listen for downloads/commands over USB connection
+        try {
+            initIndicator();
+            openDatagramConnection();
+            startSensorThresholdListeners();
+            startPeriodicalSender();
+            startMessageReceiver();
+        } catch (IOException ex) {              // A problem in reading the sensors. 
+            ex.printStackTrace();
+        }
+    }
+
+    protected void pauseApp() { 
+    }
+
+    protected void destroyApp(boolean arg0) throws MIDletStateChangeException { 
+        // TODO close connections, stop threads ...
+        endIndicator();            
+        stopSensorThresholdListeners();
+        stopPeriodicalSender();
+    }
+    
+    private void endIndicator(){
+        // TODO        
+    }
+    
+    private void stopSensorThresholdListeners(){
+        // TODO
+    }
+
+    private void stopPeriodicalSender(){
+        // TODO
+    }
+
+    public void thresholdExceeded(IAccelerometer3D accel, int axis, double val, boolean relative) {
+        // TODO
+    }
+
+    public void thresholdChanged(IAccelerometer3D accel, int axis, double low, double high, boolean relative) {
+        // TODO
+    }
+
+    public void thresholdExceeded(ITemperatureInput temp, double val, boolean inCelsius) {
+        // TODO
+    }
+
+    public void thresholdChanged(ITemperatureInput temp, double low, double high, boolean inCelsius) {
+        // TODO
+    }
+
+    public void thresholdExceeded(ILightSensor light, int val) {
+        // TODO
+    }
+
+    public void thresholdChanged(ILightSensor light, int low, int high) {
+        // TODO
+    }
+}
