89e924dec6b44520cb4f8860ba8ca4d303b2cb82
touseau <touseau@3fbdb089-5876-4d40-aeb2-f6ac8130dd8c>
2008-10-13 14:28:08 +0000

    iPOJOized sudden motion producer

--- sandbox/touseau/suddenmotionproducer/src/main/java/org/ow2/aspirerfid/sensor/motionsensorproducer/MotionSensorProducer.java
+++ sandbox/touseau/suddenmotionproducer/src/main/java/org/ow2/aspirerfid/sensor/motionsensorproducer/MotionSensorProducer.java
@@ -19,9 +19,15 @@
 
 package org.ow2.aspirerfid.sensor.motionsensorproducer;
 
-import org.osgi.service.component.ComponentContext;
+import java.util.Hashtable;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.Constants;
+import org.osgi.framework.ServiceRegistration;
 import org.osgi.service.wireadmin.Producer;
 import org.osgi.service.wireadmin.Wire;
+import org.osgi.service.wireadmin.WireConstants;
+import org.osgi.util.measurement.Measurement;
 
 import sms.Unimotion;
 
@@ -34,9 +40,15 @@ public class MotionSensorProducer implements Producer, Runnable {
 	/**
 	 * Wires connected to this producer
 	 */
-	private Wire[] wires;
+	private Wire[] m_wires;
 	
-	private final Class[] flavors = {int.class};
+	private Class[] m_flavors;
+	
+	private BundleContext m_bc;
+	
+	private ServiceRegistration m_sr;
+	
+	private final String PID = "org.ow2.aspirerfid.sensor.motionsensorproducer.MotionSensorProducer";
 	
 	private boolean end;
 
@@ -45,20 +57,51 @@ public class MotionSensorProducer implements Producer, Runnable {
 	 */
 	private final long UPDATE_DELAY = 500;
 	
-	public void activate(ComponentContext componentContext) {
-		System.out.println("MotionSensorProducer activated");		
+	public void activate() {
+		System.out.println("MotionSensorProducer activated");
+		
+		m_flavors = new Class[]{Measurement[].class};
+		
+		// publish the producer service
+		Hashtable<String,Object> props = new Hashtable<String, Object>();
+		props.put(Constants.SERVICE_PID, this.PID);
+		props.put(WireConstants.WIREADMIN_PRODUCER_FLAVORS, m_flavors);
+		m_sr = m_bc.registerService(Producer.class.getName(), this, props);
+		
 		end = false;
 		Thread t = new Thread (this,"Accelerometer Producer");
 		t.start();
 	}
 
-	public void deactivate(ComponentContext componentContext) {
+	public void deactivate() {
 		end = true;
+		m_sr.unregister();
 		System.out.println("MotionSensorProducer deactivated");
 	}
 	
+	public MotionSensorProducer(BundleContext bc){
+		m_bc = bc;
+	}
+	
+//	/**
+//	 * Callback method called by the iPOJO framework at instantiation time
+//	 * @param flavors the new flavor properties
+//	 */
+//	public void setFlavors(String[] flavors){
+//		Class[] temp = new Class[flavors.length];
+//		for (int i=0; i < flavors.length; i++){
+//			try {
+//				temp[i] = m_bc.getBundle().loadClass(flavors[i]);
+//			} catch (ClassNotFoundException e) {
+//				e.printStackTrace(); // TODO msg plus explicite
+//			}
+//		}
+//		m_flavors = temp;
+//		
+//	}
+	
 	public void consumersConnected(Wire[] wires) {
-		this.wires = wires;
+		m_wires = wires;
 	}
 
 	public Object polled(Wire wire) {
@@ -66,14 +109,17 @@ public class MotionSensorProducer implements Producer, Runnable {
 	}
 
 	public void run() {
-		end = false;
 		int[] accelerometerValues;
+		Measurement[] measurements = new Measurement[3];
 		
 		while (!end){
 			accelerometerValues = Unimotion.getSMSArray();
-			if (wires != null){
-				for (Wire wire : wires){
-					wire.update(accelerometerValues);
+			for (int i = 0; i < 3; i++){
+				measurements[i] = new Measurement(accelerometerValues[i]);
+			}
+			if (m_wires != null){
+				for (Wire wire : m_wires){
+					wire.update(measurements);
 				}
 			}
 			
