2caa0986bd506d99f3cc2df7c108cf98768c530c
fornacif <fornacif@3fbdb089-5876-4d40-aeb2-f6ac8130dd8c>
2008-11-28 15:18:57 +0000

    Update some licence headers

--- sandbox/fornacif/bundles/devices/serial-communicator/src/main/java/org/ow2/aspirerfid/device/serialcommunicator/impl/SerialCommunicatorImpl.java
+++ sandbox/fornacif/bundles/devices/serial-communicator/src/main/java/org/ow2/aspirerfid/device/serialcommunicator/impl/SerialCommunicatorImpl.java
@@ -1,22 +1,20 @@
 /**
- * JOnAS: Java(TM) Open Application Server
- * Copyright (C) 2008 Bull S.A.S.
- * Contact: jonas-team@ow2.org
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
- * USA
+ * Copyright 2008, Aspire
+ * 
+ * This library is free software; you can redistribute it and/or modify it 
+ * under the terms of the GNU Lesser General Public License as published by 
+ * the Free Software Foundation (the "LGPL"); either version 2.1 of the 
+ * License, or (at your option) any later version. If you do not alter this 
+ * notice, a recipient may use your version of this file under either the 
+ * LGPL version 2.1, or (at his option) any later version.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License 
+ * along with this library; if not, write to the Free Software Foundation, 
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ * 
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY 
+ * KIND, either express or implied. See the GNU Lesser General Public 
+ * License for the specific language governing rights and limitations.
  *
  * --------------------------------------------------------------------------
  * $Id$
