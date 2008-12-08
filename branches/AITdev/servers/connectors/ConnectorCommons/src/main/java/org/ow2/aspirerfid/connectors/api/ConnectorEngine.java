9d0eec9ed65366467aae5d198826ff51833928a6
nektarios <nektarios@3fbdb089-5876-4d40-aeb2-f6ac8130dd8c>
2008-12-08 10:07:02 +0000

    Common library for AspireRFID connectors

---
+++ branches/AITdev/servers/connectors/ConnectorCommons/src/main/java/org/ow2/aspirerfid/connectors/api/ConnectorEngine.java
@@ -0,0 +1,43 @@
+/**
+ * Copyright © 2008-2010, Aspire 
+ * 
+ * Aspire is free software; you can redistribute it and/or 
+ * modify it under  the terms of the GNU Lesser General Public 
+ * License version 2.1 as published by the Free Software Foundation (the 
+ * "LGPL"). 
+ * 
+ * You should have received a copy of the GNU Lesser General Public 
+ * License along with this library in the file COPYING-LGPL-2.1; if not, write to the Free Software 
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA. 
+ * 
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY 
+ * OF ANY KIND, either express or implied. See the GNU Lesser General Public 
+ * License for the specific language governing rights and limitations. 
+ * 
+ */
+
+package org.ow2.aspirerfid.connectors.api;
+
+/**
+ * This interface define the endpoints that the connector client should invoke
+ * when it wants to register or unregister for a specific transaction.
+ * @author Nektarios Leontiadis (nele@ait.edu.gr)
+ *
+ */
+public interface ConnectorEngine {
+
+    /**
+     * Register an observator for a transaction
+     * @param tid Transaction identifier 
+     * @param sid Subscription identifier
+     * @return True if the operations is completed without errors; false otherwise
+     */
+    public boolean startObservingTransaction(String tid, String sid);
+    
+    /**
+     * Unsubscribe an observator for an already registered transaction 
+     * @param sid The subscription identifier that should be unsubscribed
+     * @return True if the operation completes withour errors; false otherwise (e.g. there is no such subscription)
+     */
+    public boolean stopObservingTransaction(String sid);
+}
